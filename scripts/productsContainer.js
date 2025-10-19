c9e40931f70cff3a399d5f188d649c47689e85b0
Matthew Larrobis <105086636+MoogsKotobuki@users.noreply.github.com>
2025-10-19 13:55:30 +0800

    Product Overview2

--- scripts/productsContainer.js
+++ scripts/productsContainer.js
@@ -5,20 +5,20 @@ for(let i = 0;i<10;i++)
     showProduct("Chinese Headphones","$1.00")
 }
 
-function showProduct(Title,Description)
+function showProduct(Title,Price)
 {
-container.innerHTML += `
+  container.innerHTML += `
 
-<div class="card m-2" style="width: 18rem;">
-  <img src="../image/placeHolder2.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${Title}</h5>
-    <p class="card-text">
-    ${Description}</p>
-    <a href="#" class="btn btn-primary">Show Product</a>
-    <a href="#" class="btn btn-primary">Add</a>
+  <div class="card m-2 custom-primaryColor" style="width: 18rem;">
+    <img src="../image/placeHolder2.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"><strong>${Title}</strong></h5>
+      <p class="card-text">
+      ${Price}</p>
+      <a href="../pages/productOverview.html" class="btn btn-primary">Show Product</a>
+      <a href="#" class="btn btn-primary">Add</a>
+    </div>
   </div>
-</div>
 
 `
 }
