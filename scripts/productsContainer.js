11ee094cc7023128fee0a1f8ba186fc623d74cad
Matthew Larrobis <105086636+MoogsKotobuki@users.noreply.github.com>
2025-10-24 10:50:49 +0800

    Json 2

--- scripts/productsContainer.js
+++ scripts/productsContainer.js
@@ -1,34 +1,35 @@
 const container = document.getElementById("pContainer")
 
 
-async function loadDb()
+async function loadDB()
 {
   const response = await fetch("../database/products.json");
   const db = await response.json();
-  console.log(db)
   return db;
 }
+const database = await loadDB();
+console.log(database)
 
-for(let i = 0;i<10;i++)
+database.forEach(db => {
+  showProduct(db.title,db.price[0],db.image)
+  
+});
+
+
+function showProduct(Title,Price,image)
 {
-    showProduct("Chinese Headphones","$1.00")
-}
-
-const db = await loadDb();
-
-function showProduct(Title,Price)
-{
-  container.innerHTML += `
+  container.innerHTML += 
+  `
 
   <div class="card m-2 custom-primaryColor" style="width: 18rem;">
-    <img src="../image/placeHolder2.jpg" class="card-img-top" alt="...">
-    <a class="card-body ">
+    <img src="${image}" class="card-img-top product-img" alt="...">
+    <div class="card-body ">
       <h5 class="card-title"><strong>${Title}</strong></h5>
       <p class="card-text">
-      ${Price}</p>
+      ₱${Price}</p>
       <a href="../pages/productOverview.html" class="btn btn-primary">Show Product</a>
       <a href="#" class="btn btn-primary">Add</a>
-    </a>
+    </div>
   </div>
 
 `
