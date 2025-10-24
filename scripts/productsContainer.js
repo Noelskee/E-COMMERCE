48d8adee57049b7d36b8ebddb7281f32e05ac495
Matthew Larrobis <105086636+MoogsKotobuki@users.noreply.github.com>
2025-10-24 10:15:49 +0800

    Json Set

--- scripts/productsContainer.js
+++ scripts/productsContainer.js
@@ -1,9 +1,20 @@
 const container = document.getElementById("pContainer")
 
+
+async function loadDb()
+{
+  const response = await fetch("../database/products.json");
+  const db = await response.json();
+  console.log(db)
+  return db;
+}
+
 for(let i = 0;i<10;i++)
 {
     showProduct("Chinese Headphones","$1.00")
 }
+
+const db = await loadDb();
 
 function showProduct(Title,Price)
 {
