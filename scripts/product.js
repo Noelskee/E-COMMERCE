0f70c2423e55eec24fb29fe46d846287edd2768a
Matthew Larrobis <105086636+MoogsKotobuki@users.noreply.github.com>
2025-11-02 13:12:10 +0800

    AAAA

--- scripts/product.js
+++ scripts/product.js
@@ -17,10 +17,17 @@ const description = document.getElementById('description');
 const imag = document.getElementById('image');
 const option = document.getElementById('option');
 
+
+let priceIndex = 0;
+
 let data = database[id]
+
+let Price = data.price[priceIndex]
+
+
 console.log(data)
 title.innerHTML = data.title;
-price.innerHTML = '₱' + data.price[0];
+price.innerHTML = '₱' + Price;
 description.innerHTML = data.description;
 option.innerHTML = data.optionName + ':';
 
@@ -28,17 +35,26 @@ imag.src = data.image
 
 optionInit(data);
 
+
+
+function changePrice(index)
+{
+  priceIndex = index;
+  Price = data.price[priceIndex];
+  price.innerHTML = '₱' + Price;
+}
+
 function optionInit(O)
 {
   const optionHoler = document.getElementById("optionHolder");
   let options = O.options;
     console.log(options);
-
+  let indexCount = 0;
   options.forEach(A => {
     
     optionHoler.innerHTML += `
-    <button class="btn btn-primary p-3 m-2 " type="button">${A}</button>
+    <button class="btn btn-primary p-3 m-2 " type="button" id="${indexCount}" onclick="hello();">${A}</button>
 `
-
+    indexCount++;
   });
 }
