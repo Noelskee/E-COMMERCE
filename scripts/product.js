ecdee3b1e33e4051e90aca1aaea6e284fdbc8352
Matthew Larrobis <105086636+MoogsKotobuki@users.noreply.github.com>
2025-11-02 12:14:57 +0800

    Product2

--- scripts/product.js
+++ scripts/product.js
@@ -15,10 +15,30 @@ const title = document.getElementById('title');
 const price = document.getElementById('price');
 const description = document.getElementById('description');
 const imag = document.getElementById('image');
+const option = document.getElementById('option');
 
 let data = database[id]
 console.log(data)
 title.innerHTML = data.title;
 price.innerHTML = '₱' + data.price[0];
 description.innerHTML = data.description;
+option.innerHTML = data.optionName + ':';
+
 imag.src = data.image
+
+optionInit(data);
+
+function optionInit(O)
+{
+  const optionHoler = document.getElementById("optionHolder");
+  let options = O.options;
+    console.log(options);
+
+  options.forEach(A => {
+    
+    optionHoler.innerHTML += `
+    <button class="btn btn-primary p-3 m-2 " type="button">${A}</button>
+`
+
+  });
+}
